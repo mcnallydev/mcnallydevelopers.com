1bf25270904b2e288715fc63ffebd8bb5a3732b6
Paulo McNally <paulomcnally@gmail.com>
2017-03-05 04:25:44 -0600

    enable analytics

--- app/middleware/locale.js
+++ app/middleware/locale.js
@@ -1,7 +1,10 @@
 'use strict';
 module.exports = function (req, res, next) {
-    var locale = req.cookies && req.cookies.locale;
-    //Set the locality for this response. The template will pick the appropriate bundle
-    res.locals.locale = locale;
-    next();
+  var locale = req.cookies && req.cookies.locale;
+  //Set the locality for this response. The template will pick the appropriate bundle
+  res.locals.locale = locale;
+
+  res.locals.isProduction = (process.env.NODE_ENV === 'production');
+
+  next();
 };
